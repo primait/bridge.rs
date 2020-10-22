5b0f5c61440d8a9a1a91b3bfe8124814f2f6b961
Matteo Giachino <matteog@gmail.com>
2020-10-22 14:07:17 +0200

    adds rest request

--- src/v2/request/mod.rs
+++ src/v2/request/mod.rs
@@ -14,7 +14,9 @@ mod rest;
 
 pub struct Request;
 
+/// Useful type to construct new requests
 impl Request {
+    /// Create a new GraphQL request
     pub fn graphql<S: Serialize>(
         bridge: &Bridge,
         graphql_body: impl Into<GraphQLBody<S>>,
@@ -22,22 +24,32 @@ impl Request {
         GraphQLRequest::new(bridge, graphql_body)
     }
 
+    /// Create a rest request
+    pub fn rest(bridge: &Bridge) -> RestRequest {
+        Self::get(bridge)
+    }
+
+    /// Create a new GET request
     pub fn get(bridge: &Bridge) -> RestRequest {
         RestRequest::new(bridge)
     }
 
+    /// Create a new POST request
     pub fn post(bridge: &Bridge) -> RestRequest {
         RestRequest::new(bridge).method(Method::POST)
     }
 
+    /// Create a new PATCH request
     pub fn patch(bridge: &Bridge) -> RestRequest {
         RestRequest::new(bridge).method(Method::PATCH)
     }
 
+    /// Create a new DELETE request
     pub fn delete(bridge: &Bridge) -> RestRequest {
         RestRequest::new(bridge).method(Method::DELETE)
     }
 
+    /// Create a new PUT request
     pub fn put(bridge: &Bridge) -> RestRequest {
         RestRequest::new(bridge).method(Method::PUT)
     }
