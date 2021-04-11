5768936149452b0c6ea92b18bd8b539486fb09df
Matteo Giachino <matteog@gmail.com>
2021-04-11 23:21:27 +0200

    api flex

--- src/token_dispenser.rs
+++ src/token_dispenser.rs
@@ -25,6 +25,16 @@ pub enum TokenDispenserMessage {
     },
 }
 
+impl TokenDispenserMessage {
+    pub fn get_token(respond_to: oneshot::Sender<Option<String>>) -> Self {
+        Self::GetToken { respond_to }
+    }
+
+    pub fn token_needs_refresh(respond_to: oneshot::Sender<bool>) -> Self {
+        Self::TokenNeedsRefresh { respond_to }
+    }
+}
+
 impl TokenDispenser {
     pub fn new(
         endpoint: Url,
@@ -88,29 +98,43 @@ impl TokenDispenserHandle {
             let mut ticker = tokio::time::interval(duration);
             loop {
                 ticker.tick().await;
-                let (send, recv) = oneshot::channel();
-                let _ = interval_sender
-                    .send(TokenDispenserMessage::TokenNeedsRefresh { respond_to: send })
-                    .await;
-                let needs_refresh = recv.await.expect("task has been killed");
-                if needs_refresh {
-                    let _ = interval_sender
-                        .send(TokenDispenserMessage::RefreshToken)
-                        .await;
+                if Self::needs_refresh(&interval_sender).await {
+                    Self::do_cast(&interval_sender, TokenDispenserMessage::RefreshToken).await;
                 }
             }
         });
     }
 
     pub async fn refresh_token(&self) {
-        let msg = TokenDispenserMessage::RefreshToken;
-        let _ = self.sender.send(msg).await;
+        self.cast(TokenDispenserMessage::RefreshToken).await;
     }
 
     pub async fn get_token(&self) -> Option<String> {
+        self.call(TokenDispenserMessage::get_token).await
+    }
+
+    async fn needs_refresh(sender: &mpsc::Sender<TokenDispenserMessage>) -> bool {
+        Self::do_call(sender, TokenDispenserMessage::token_needs_refresh).await
+    }
+
+    async fn call<T>(&self, msg: impl Fn(oneshot::Sender<T>) -> TokenDispenserMessage) -> T {
+        Self::do_call(&self.sender, msg).await
+    }
+
+    async fn do_call<T>(
+        sender: &mpsc::Sender<TokenDispenserMessage>,
+        msg: impl Fn(oneshot::Sender<T>) -> TokenDispenserMessage,
+    ) -> T {
         let (send, recv) = oneshot::channel();
-        let msg = TokenDispenserMessage::GetToken { respond_to: send };
-        let _ = self.sender.send(msg).await;
+        let _ = sender.send(msg(send)).await;
         recv.await.expect("get_token task has been killed")
+    }
+
+    async fn cast(&self, msg: TokenDispenserMessage) {
+        Self::do_cast(&self.sender, msg);
+    }
+
+    async fn do_cast(sender: &mpsc::Sender<TokenDispenserMessage>, msg: TokenDispenserMessage) {
+        let _ = sender.send(msg).await;
     }
 }
