4dff0fac2a46121d163e7b8268173f389b2894fe
Matteo Giachino <matteog@gmail.com>
2021-04-22 16:06:54 +0200

    jwks checks async implementation

--- src/token_dispenser/jwks.rs
+++ src/token_dispenser/jwks.rs
@@ -3,16 +3,25 @@ use crate::prelude::{
     PrimaBridgeResult,
 };
 
-use alcoholic_jwt::JWKS;
+use alcoholic_jwt::{token_kid, JWKS};
 use reqwest::Url;
 use tokio::sync::{mpsc, oneshot};
 
 pub enum TokenCheckerMsg {
     FetchJwks,
     Check {
-        token: String,
+        token: JwtToken,
         respond_to: oneshot::Sender<bool>,
     },
+}
+
+impl TokenCheckerMsg {
+    pub fn check(token: impl Into<JwtToken>) -> impl FnOnce(oneshot::Sender<bool>) -> Self {
+        move |respond_to| Self::Check {
+            token: token.into(),
+            respond_to,
+        }
+    }
 }
 
 pub struct TokenChecker {
@@ -20,6 +29,24 @@ pub struct TokenChecker {
     jwks_url: Url,
     jwks: Option<JWKS>,
     receiver: mpsc::Receiver<TokenCheckerMsg>,
+}
+
+pub struct JwtToken(String);
+
+impl JwtToken {
+    fn get_kid(&self) -> Option<String> {
+        token_kid(&self.0).ok().flatten()
+    }
+
+    pub fn is_signed_by(&self, jwks: &JWKS) -> bool {
+        self.get_kid().and_then(|kid| jwks.find(&kid)).is_some()
+    }
+}
+
+impl From<String> for JwtToken {
+    fn from(token_string: String) -> Self {
+        Self(token_string)
+    }
 }
 
 impl TokenChecker {
@@ -41,10 +68,20 @@ impl TokenChecker {
     async fn handle_message(&mut self, msg: TokenCheckerMsg) {
         match msg {
             TokenCheckerMsg::FetchJwks => {
-                self.jwks = self.fetch_jwks().await.ok();
+                // if the request, for any reason, fails, we simply skip the update
+                // We already handle the "missing jwks" case by always responding true
+                if let Some(jwks) = self.fetch_jwks().await.ok() {
+                    self.jwks = Some(jwks);
+                }
             }
-            TokenCheckerMsg::Check { respond_to, .. } => {
-                let _ = respond_to.send(true);
+            TokenCheckerMsg::Check { token, respond_to } => {
+                // if we don't have jwks, or the token doesn't have a key, we simply return true
+                let _ = respond_to.send(
+                    self.jwks
+                        .as_ref()
+                        .map(|jwks| token.is_signed_by(jwks))
+                        .unwrap_or(true),
+                );
             }
         }
     }
