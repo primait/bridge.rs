43e612cbaad43e4fb98a1cb0ad42f351aa561f2a
Matteo Giachino <matteog@gmail.com>
2022-01-27 14:38:35 +0100

    update base image and simple warnings

--- tests/async_auth0/graphql.rs
+++ tests/async_auth0/graphql.rs
@@ -72,11 +72,13 @@ async fn request_with_custom_headers() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Debug)]
 struct GqlResponse {
     hero: Hero,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Debug)]
 struct Hero {
     name: String,
@@ -84,6 +86,7 @@ struct Hero {
     friends: Vec<Option<Friend>>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Debug)]
 struct Friend {
     id: String,
