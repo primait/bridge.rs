ea48101ced8f3a2fe2c358f2d0bbbf49931ffdc6
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-06-14 12:25:39 +0200

    wip: use new cbc crate

--- src/auth0/cache/crypto.rs
+++ src/auth0/cache/crypto.rs
@@ -1,20 +1,29 @@
-use aes::Aes256 as Aes256Alg;
-use block_modes::block_padding::Pkcs7;
-use block_modes::{BlockMode, Cbc};
+use aes::{
+    cipher::{block_padding::Pkcs7, BlockDecryptMut, BlockEncryptMut, KeyIvInit},
+    Aes256,
+};
+use cbc::{Decryptor, Encryptor};
 use serde::{Deserialize, Serialize};
 
 use crate::auth0::errors::Auth0Error;
 
-type Aes256 = Cbc<Aes256Alg, Pkcs7>;
+type Aes256Enc = Encryptor<Aes256>;
+type Aes256Dec = Decryptor<Aes256>;
 
 const IV: &str = "301a9e39735f4646";
 
 pub fn encrypt<T: Serialize>(value_ref: &T, token_encryption_key_str: &str) -> Result<Vec<u8>, Auth0Error> {
     let json: String = serde_json::to_string(value_ref)?;
+
     // `unwrap` here is fine because `IV` is set here and the only error returned is: `InvalidKeyIvLength`
     // and this must never happen
-    let cipher: Aes256 = Aes256::new_from_slices(token_encryption_key_str.as_bytes(), IV.as_bytes()).unwrap();
-    Ok(cipher.encrypt_vec(json.as_bytes()))
+    let mut buf = [0u8; 48];
+    buf[..json.len()].copy_from_slice(json.as_bytes());
+    let ct = Aes256Enc::new(token_encryption_key_str.as_bytes().into(), IV.as_bytes().into())
+        .encrypt_padded_mut::<Pkcs7>(&mut buf, json.len())
+        .unwrap();
+
+    Ok(ct.to_vec())
 }
 
 pub fn decrypt<T>(token_encryption_key_str: &str, encrypted: &[u8]) -> Result<T, Auth0Error>
@@ -23,7 +32,10 @@ where
 {
     // `unwrap` here is fine because `IV` is set here and the only error returned is: `InvalidKeyIvLength`
     // and this must never happen
-    let cipher: Aes256 = Aes256::new_from_slices(token_encryption_key_str.as_bytes(), IV.as_bytes()).unwrap();
-    let decrypted: Vec<u8> = cipher.decrypt_vec(encrypted)?;
-    Ok(serde_json::from_slice(decrypted.as_slice())?)
+    let mut buf = Vec::from(encrypted);
+    let pt = Aes256Dec::new(token_encryption_key_str.as_bytes().into(), IV.as_bytes().into())
+        .decrypt_padded_mut::<Pkcs7>(&mut buf)
+        .unwrap();
+
+    Ok(serde_json::from_slice(pt)?)
 }
