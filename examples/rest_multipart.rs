4bb9bc7ed5054f3d5c1a8cd984a6741c8926a219
William Venner <william@venner.io>
2022-09-16 14:26:34 +0100

    Add example of REST multipart

---
+++ examples/rest_multipart.rs
@@ -0,0 +1,46 @@
+//! Rest example
+//!
+//! In this example there's the explanation on how to achieve a rest call to an endpoint and parsing
+//! the response. The response is the person fetched from persons api with id=1.
+//!
+//! Run this to run the example:
+//!
+//! ```shell
+//! cargo run --example graphql_multipart
+//! ```
+use std::{collections::HashSet, iter::FromIterator};
+
+use prima_bridge::{prelude::*, MultipartFile, MultipartFormFileField, RestMultipart};
+
+const URL: &str = "https://youtube.com/";
+
+#[tokio::main]
+async fn main() {
+    let bridge: Bridge = Bridge::builder().build(URL.parse().unwrap());
+
+    let multipart = RestMultipart::multiple(HashSet::from_iter(
+        [
+            MultipartFormFileField::new(
+                "file0",
+                MultipartFile::new("Hello, world!")
+                    .with_name("hello_world.txt")
+                    .with_mime_type("text/plain"),
+            ),
+            MultipartFormFileField::new(
+                "file1",
+                MultipartFile::new("Goodbye, world!")
+                    .with_name("goodbye_world.dat")
+                    .with_mime_type("application/octet-stream"),
+            ),
+        ]
+        .into_iter(),
+    ));
+
+    let response: Response = RestRequest::new_with_multipart(&bridge, multipart)
+        .to("upload")
+        .send()
+        .await
+        .expect("http rest request error");
+
+    assert_eq!(response.status_code().as_u16(), 200);
+}
