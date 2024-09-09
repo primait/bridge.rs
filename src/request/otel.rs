488a6334ba92d6d490b133a73bb6d1e16faa5843
Simone Cottini <cottini.simone@gmail.com>
2024-09-09 16:17:01 +0200

    Bump otel group 0.24 (#168)
    * Bumping otel to 0.24
    
    * v0.16.7 & update changelog
    
    * Fix cargo make docs command

--- src/request/otel.rs
+++ src/request/otel.rs
@@ -1,6 +1,3 @@
-#[cfg(not(feature = "tracing_opentelemetry_0_23"))]
-#[cfg(not(feature = "tracing_opentelemetry_0_22"))]
-#[cfg(not(feature = "tracing_opentelemetry_0_21"))]
 #[cfg(feature = "tracing_opentelemetry_0_20")]
 mod otel_0_20 {
     pub use opentelemetry_0_20_pkg::{
@@ -14,8 +11,6 @@ mod otel_0_20 {
     }
 }
 
-#[cfg(not(feature = "tracing_opentelemetry_0_23"))]
-#[cfg(not(feature = "tracing_opentelemetry_0_22"))]
 #[cfg(feature = "tracing_opentelemetry_0_21")]
 mod otel_0_21 {
     pub use opentelemetry_0_21_pkg::propagation::{Injector, TextMapPropagator};
@@ -27,7 +22,6 @@ mod otel_0_21 {
     }
 }
 
-#[cfg(not(feature = "tracing_opentelemetry_0_23"))]
 #[cfg(feature = "tracing_opentelemetry_0_22")]
 mod otel_0_22 {
     pub use opentelemetry_0_22_pkg::propagation::{Injector, TextMapPropagator};
@@ -50,20 +44,28 @@ mod otel_0_23 {
     }
 }
 
-#[cfg(not(feature = "tracing_opentelemetry_0_23"))]
-#[cfg(not(feature = "tracing_opentelemetry_0_22"))]
-#[cfg(not(feature = "tracing_opentelemetry_0_21"))]
+#[cfg(feature = "tracing_opentelemetry_0_24")]
+mod otel_0_24 {
+    pub use opentelemetry_0_24_pkg::propagation::{Injector, TextMapPropagator};
+    pub use opentelemetry_sdk_0_24_pkg::propagation::TraceContextPropagator;
+    pub use tracing_opentelemetry_0_25_pkg::OpenTelemetrySpanExt;
+
+    pub fn inject_context(injector: &mut dyn Injector) {
+        TraceContextPropagator::new().inject_context(&tracing::Span::current().context(), injector);
+    }
+}
+
 #[cfg(feature = "tracing_opentelemetry_0_20")]
 pub use otel_0_20::inject_context;
 
-#[cfg(not(feature = "tracing_opentelemetry_0_23"))]
-#[cfg(not(feature = "tracing_opentelemetry_0_22"))]
 #[cfg(feature = "tracing_opentelemetry_0_21")]
 pub use otel_0_21::inject_context;
 
-#[cfg(not(feature = "tracing_opentelemetry_0_23"))]
 #[cfg(feature = "tracing_opentelemetry_0_22")]
 pub use otel_0_22::inject_context;
 
 #[cfg(feature = "tracing_opentelemetry_0_23")]
 pub use otel_0_23::inject_context;
+
+#[cfg(feature = "tracing_opentelemetry_0_24")]
+pub use otel_0_24::inject_context;
