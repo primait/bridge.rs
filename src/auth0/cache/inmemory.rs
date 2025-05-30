e073578d3b7450fc8086ff27c9db2dcde3009993
Cristiano Piemontese <cristiano.piemontese@prima.it>
2025-05-30 11:52:35 +0200

    refactor: simplify in memory cache

--- src/auth0/cache/inmemory.rs
+++ src/auth0/cache/inmemory.rs
@@ -8,31 +8,25 @@ use super::CacheError;
 #[derive(Default, Clone, Debug)]
 pub struct InMemoryCache {
     key_value: DashMap<String, Token>,
-    service_name: String,
-}
-
-impl InMemoryCache {
-    pub fn new(service_name: String) -> Self {
-        Self {
-            key_value: DashMap::default(),
-            service_name,
-        }
-    }
 }
 
 #[async_trait::async_trait]
 impl Cache for InMemoryCache {
     async fn get_token(&self, client_id: &str, aud: &str) -> Result<Option<Token>, CacheError> {
-        let key = super::token_key(&self.service_name, client_id, aud);
+        let key = token_key(client_id, aud);
         let token = self.key_value.get(key.as_str()).map(|v| v.to_owned());
         Ok(token)
     }
 
     async fn put_token(&self, client_id: &str, aud: &str, token: &Token) -> Result<(), CacheError> {
-        let key = super::token_key(&self.service_name, client_id, aud);
+        let key = token_key(client_id, aud);
         let _ = self.key_value.insert(key, token.clone());
         Ok(())
     }
+}
+
+fn token_key(client_id: &str, audience: &str) -> String {
+    format!("{}:{}", client_id, audience)
 }
 
 #[cfg(test)]
@@ -47,7 +41,7 @@ mod tests {
         let audience = "audience".to_string();
         let service = "service".to_string();
 
-        let cache = InMemoryCache::new(service);
+        let cache = InMemoryCache::default();
 
         let result: Option<Token> = cache.get_token(&client_id, &audience).await.unwrap();
         assert!(result.is_none());
