10291585b1c001738bd2cf2250d402efbf3a3f6e
Alessandro Campeis <12007735+campeis@users.noreply.github.com>
2025-06-11 14:40:12 +0200

    lint: fix fmt

--- src/auth0/client.rs
+++ src/auth0/client.rs
@@ -1,5 +1,5 @@
 use crate::auth0::Auth0Error;
-use chrono::{DateTime, Utc, Duration};
+use chrono::{DateTime, Duration, Utc};
 use reqwest::{Client, Response, Url};
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
