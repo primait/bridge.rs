214e148c6bb1495d46e1b4b38989c50e47d6a70c
Cristiano Piemontese <cristiano.piemontese@prima.it>
2025-10-30 15:55:52 +0100

    feat: support otel 0.31

--- src/request/otel.rs
+++ src/request/otel.rs
@@ -1,71 +1,15 @@
-#[cfg(feature = "tracing_opentelemetry_0_21")]
-mod otel_crates {
-    pub use opentelemetry_0_21_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_21_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_22_pkg as tracing_opentelemetry;
-}
-
-#[cfg(feature = "tracing_opentelemetry_0_22")]
-mod otel_crates {
-    pub use opentelemetry_0_22_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_22_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_23_pkg as tracing_opentelemetry;
-}
-
-#[cfg(feature = "tracing_opentelemetry_0_23")]
-mod otel_crates {
-    pub use opentelemetry_0_23_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_23_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_24_pkg as tracing_opentelemetry;
-}
-
-#[cfg(feature = "tracing_opentelemetry_0_24")]
-mod otel_crates {
-    pub use opentelemetry_0_24_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_24_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_25_pkg as tracing_opentelemetry;
-}
-
-#[cfg(feature = "tracing_opentelemetry_0_25")]
-mod otel_crates {
-    pub use opentelemetry_0_25_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_25_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_26_pkg as tracing_opentelemetry;
-}
-
-#[cfg(feature = "tracing_opentelemetry_0_26")]
-mod otel_crates {
-    pub use opentelemetry_0_26_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_26_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_27_pkg as tracing_opentelemetry;
-}
-
-#[cfg(feature = "tracing_opentelemetry_0_27")]
-mod otel_crates {
-    pub use opentelemetry_0_27_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_27_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_28_pkg as tracing_opentelemetry;
-}
-
-#[cfg(feature = "tracing_opentelemetry_0_28")]
-mod otel_crates {
-    pub use opentelemetry_0_28_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_28_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_29_pkg as tracing_opentelemetry;
-}
-
-#[cfg(feature = "tracing_opentelemetry_0_29")]
-mod otel_crates {
-    pub use opentelemetry_0_29_pkg as opentelemetry;
-    pub use opentelemetry_sdk_0_29_pkg as opentelemetry_sdk;
-    pub use tracing_opentelemetry_0_30_pkg as tracing_opentelemetry;
-}
-
 #[cfg(feature = "tracing_opentelemetry_0_30")]
 mod otel_crates {
     pub use opentelemetry_0_30_pkg as opentelemetry;
     pub use opentelemetry_sdk_0_30_pkg as opentelemetry_sdk;
     pub use tracing_opentelemetry_0_31_pkg as tracing_opentelemetry;
+}
+
+#[cfg(feature = "tracing_opentelemetry_0_31")]
+mod otel_crates {
+    pub use opentelemetry_0_31_pkg as opentelemetry;
+    pub use opentelemetry_sdk_0_31_pkg as opentelemetry_sdk;
+    pub use tracing_opentelemetry_0_32_pkg as tracing_opentelemetry;
 }
 
 use otel_crates::*;
